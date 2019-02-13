82b801736932c1167abb89513905634195b78c01
luisen <luisenrique.penalver@gmail.com>
2019-02-13 18:12:50 +0100

    Exercises from the session 7.

--- session-7/exercise-2.py
+++ session-7/exercise-2.py
@@ -1,4 +1,4 @@
-#Programming our client
+# Programming our client
 
 import socket
 
@@ -8,8 +8,6 @@ print("Socket created")
 PORT = 8083
 IP = "212.128.253.110"
 
-
-
 while True:
     # We create sockets for communicating with the server
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,7 +16,7 @@ while True:
     send_msg = input("Please enter your message: ")
     s.send(str.encode(send_msg))
 
-    #Receiving a message from the server
+    # Receiving a message from the server
     msg = s.recv(2048).decode("utf-8")
     print("MESSAGE FROM THE SERVER:")
     print(msg)
