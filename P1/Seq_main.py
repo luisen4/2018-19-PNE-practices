3d02afc5843fb05b6b0680057585be2a823e99e7
luisen <luisenrique.penalver@gmail.com>
2019-02-13 12:50:32 +0100

    Project 1.

--- P1/Seq_main.py
+++ P1/Seq_main.py
@@ -16,8 +16,3 @@ for index in range(len(sequences)):
     print("Bases count: A:{}, T:{}, C:{}, G:{}".format(sequences[index].count("A"), sequences[index].count("T"), sequences[index].count("C"), sequences[index].count("G")))
     print("Bases percentage: A:{}%, T:{}%, C:{}%, G:{}%".format(sequences[index].perc("A"), sequences[index].perc("T"), sequences[index].perc("C"), sequences[index].perc("G")))
     print("\n")
-
-# Sequence 1: AGTACACTGGT
-# Length: 11
-# Bases count: A:3, T:3, C:2, G:3
-# Bases percentage: A:27.3%, T:27.3%, C:18.2%, G:27.3%
